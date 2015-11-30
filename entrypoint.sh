60c6f2e9db48c35238369a1c1b31da856f791d6b
Benjamin Davison <ben.davison@7digital.com>
2015-11-30 16:17:57 +0000

    Adding random broker ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export KAFKA_ADVERTISED_HOST_NAME=$(curl --retry 5 --connect-timeout 3 -s 169.254.169.254/latest/meta-data/local-hostname)
 export LOCAL_IP=$(curl --retry 5 --connect-timeout 3 -s 169.254.169.254/latest/meta-data/local-ipv4)
+export KAFKA_BROKER_ID=($RANDOM)
 exec "$@"
